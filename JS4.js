f94194570c0238ba16def5fa03f078d4d0a6bc00
Hiro Hamada <laciferin@gmail.com>
2020-03-22 20:23:32 +0530

    Hi ther

---
+++ JS4.js
@@ -0,0 +1,28 @@
+//@22-3
+
+//!st part of the JS-4 - CLASSES
+
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  greet() {
+    console.log("Hello");
+  }
+  
+  info() {
+    // Call the greet method
+    this.greet();
+    
+    console.log(`My name is ${this.name}`);
+    console.log(`I'm ${this.age} years old`);
+  }
+}
+
+const animal = new Animal("Leo", 3);
+// Remove the line below
+
+
+animal.info();
