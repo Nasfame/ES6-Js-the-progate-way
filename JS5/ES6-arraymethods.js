108a3936c5b5c8c07fdf4677292c4731ad353169
Hiro Hamada <laciferin@gmail.com>
2020-03-22 23:57:15 +0530

    Create ES6-arraymethods.js

---
+++ JS5/ES6-arraymethods.js
@@ -0,0 +1,28 @@
+//@22-3 JS-VI
+
+//Array Methods
+
+//1.push
+const characters = ["Ken the Ninja", "Ben the Baby Ninja", "Master White"];
+
+console.log(characters);
+
+// Add the string "Birdie" to the characters array with the push method
+characters.push('Birdie');
+
+
+// Print the characters array
+console.log(characters);
+
+//2.forEach
+
+const characters = ["Ken the Ninja", "Ben the Baby Ninja", "Master White", "Birdie"];
+
+// Print all elements in the characters array by using the forEach method
+characters.forEach((character)=>{
+  console.log(character);
+});
+
+//
+
+
